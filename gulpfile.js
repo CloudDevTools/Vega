d802e7180a7d7db9135a4b140897229eee5dbf3f
yubing <ybjx1987@qq.com>
2016-10-25 23:02:59 +0800

    1、初始化工程 2、添加

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+/**
+ * Created by yubing on 2016/10/25.
+ */
+
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
+
+gulp.task("default",function(){
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest("./.tmp"));
+});
