8868b9328f44eb64f7fc7e3f08d109b83b318b90
yubing <ybjx1987@qq.com>
2016-10-25 23:14:31 +0800

    1、gulp中添加了clean任务，该任务用于删除.tmp文件夹中的全部内容 2、gulp中build任务会先调用clean任务

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,15 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
+var del = require("del");
+var vinylPaths = require('vinyl-paths');
 
-gulp.task("default",function(){
+gulp.task("clean",function(){
+    return gulp.src('.tmp/*')
+        .pipe(vinylPaths(del));
+})
+
+gulp.task("default",["clean"],function(){
     return tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest("./.tmp"));
