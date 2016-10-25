256769836d1ea069f13c5ee21a5cb98a9472e61c
yubing <ybjx1987@qq.com>
2016-10-25 23:26:42 +0800

    1、服务提供者增加了tcp连接的端口字段

--- src/ServiceProvider.ts
+++ src/ServiceProvider.ts
@@ -6,13 +6,17 @@ export class ServiceProvider{
     private _host:string;
     private _port:number;
     private _version:string;
+    private _tcp_port:number;
 
-    constructor(host:string,port:number,version:string){
+    constructor(host:string,port:number,version:string,tcp_port:number){
         if(host == null || host == ""){
             throw "Provider host name can not be empty!";
         }
         if(port <= 1000){
             throw "Provider port must big than 1000!";
+        }
+        if(tcp_port <= 1000){
+            throw "Provider tcp port must big than 1000!";
         }
         if(version == null || version == ""){
             throw "Provider version can not be empty!";
